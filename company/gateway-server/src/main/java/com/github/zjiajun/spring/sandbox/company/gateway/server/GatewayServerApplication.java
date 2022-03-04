d211e7798babbed86c6d1a7d7971a56e746af5fe
zjiajun <zhujiajun.me@gmail.com>
2022-03-04 10:18:08 +0800

    force push

---
+++ company/gateway-server/src/main/java/com/github/zjiajun/spring/sandbox/company/gateway/server/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.github.zjiajun.spring.sandbox.company.gateway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhujiajun
+ * @version 1.0
+ * @since 2022/3/4 09:53
+ */
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
